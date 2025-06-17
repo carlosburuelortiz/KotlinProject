3f6c3e0e8f925d851caaada61fdc7c860b986850
Carlos Buruel <charly.ob.26@gmail.com>
2025-06-17 17:20:41 -0600

    36. Configurando nuestro topBar

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,9 +1,30 @@
 package org.example.project
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Apps
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import moe.tlaster.precompose.PreComposeApp
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.path
+import moe.tlaster.precompose.navigation.rememberNavigator
+import org.example.project.data.TitleTopBarTypes
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
@@ -11,7 +32,71 @@ fun App() {
         val colors = getColorsTheme()
 
         AppTheme {
+            val navigator = rememberNavigator()
+            val titleTopBar = getTitleTopAppBar(navigator)
+            val isEditOrAddExpenses = titleTopBar != TitleTopBarTypes.DASHBOARD.value
 
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    Surface(
+                        shadowElevation = 0.dp,
+                    ) {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = titleTopBar,
+                                    fontSize = 25.sp,
+                                    color = colors.textColor,
+                                )
+                            },
+                            navigationIcon = {
+                                if (isEditOrAddExpenses) {
+                                    IconButton(
+                                        onClick = {
+                                            navigator.popBackStack()
+                                        }
+                                    ) {
+                                        Icon(
+                                            modifier = Modifier.padding(start = 16.dp),
+                                            imageVector = Icons.Default.ArrowBackIosNew,
+                                            tint = colors.textColor,
+                                            contentDescription = "Flecha regresar"
+                                        )
+                                    }
+                                } else {
+                                    Icon(
+                                        modifier = Modifier.padding(start = 16.dp),
+                                        imageVector = Icons.Default.Apps,
+                                        tint = colors.textColor,
+                                        contentDescription = "Dashboard icon"
+                                    )
+                                }
+                            }
+                        )
+                    }
+                },
+            ) {
+
+            }
         }
     }
+}
+
+@Composable
+fun getTitleTopAppBar(navigator: Navigator): String {
+    var titleTopBar = TitleTopBarTypes.DASHBOARD
+
+    val isOnAddExpense =
+        navigator.currentEntry.collectAsState(null).value?.route?.route.equals("/addExpenses/{id}")
+    if (isOnAddExpense) {
+        titleTopBar = TitleTopBarTypes.ADD
+    }
+
+    val isOnEditExpense = navigator.currentEntry.collectAsState(null).value?.path<Long>("id")
+    isOnEditExpense?.let {
+        titleTopBar = TitleTopBarTypes.EDIT
+    }
+
+    return titleTopBar.value
 }

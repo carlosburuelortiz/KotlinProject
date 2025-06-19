c3329e16a04d56d61ba65d55122eaabc79d9bd8f
Carlos Buruel <charly.ob.26@gmail.com>
2025-06-18 19:30:24 -0600

    40. expenseDetailScreen UI - 3/4

--- composeApp/src/commonMain/kotlin/org/example/project/ui/ExpensesDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/ExpensesDetailScreen.kt
@@ -1,20 +1,30 @@
 package org.example.project.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,9 +36,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import org.example.project.getColorsTheme
 import org.example.project.model.Expense
@@ -65,16 +82,108 @@ fun ExpensesDetailScreen(
     }
 
     ModalBottomSheet(
-        onDismissRequest = {},
+        onDismissRequest = {
+            scope.launch { sheetState.hide() }
+        },
         sheetState = sheetState,
     ) {
         CategoryBottomSheetContent(categoryList) {
             expenseCategory = it.name
             categorySelected = it.name
-            scope.launch {
-                sheetState.hide()
-            }
         }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(vertical = 16.dp, horizontal = 16.dp).background(Color.Red)
+    ) {
+        ExpanseAmount(
+            priceContent = price,
+            onPriceChange = {
+                price = it
+            },
+            keyboardController = keyboardController,
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+    }
+}
+
+@Composable
+private fun ExpanseAmount(
+    priceContent: Double,
+    onPriceChange: (Double) -> Unit,
+    keyboardController: SoftwareKeyboardController?,
+) {
+    val colors = getColorsTheme()
+    var text by remember { mutableStateOf("$priceContent") }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Amount",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.SemiBold,
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = colors.textColor
+            )
+            TextField(
+                modifier = Modifier.weight(1f),
+                value = text,
+                onValueChange = { newText ->
+                    val numericText = newText.filter { it.isDigit() || it == '.' }
+                    text = if (numericText.isNotEmpty() && numericText.count { it == '.' } <= 1) {
+                        try {
+                            val newValue = numericText.toDouble()
+                            onPriceChange(newValue)
+                            numericText
+                        } catch (e: NumberFormatException) {
+                            ""
+                        }
+                    } else {
+                        onPriceChange(0.0)
+                        ""
+                    }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                    }
+                ),
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    focusedTextColor = colors.textColor,
+                    unfocusedTextColor = Color.Transparent,
+                    focusedContainerColor = colors.backgroundColor,
+                    unfocusedContainerColor = Color.Transparent,
+                ),
+                textStyle = TextStyle(
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                )
+            )
+
+            Text(
+                text = "USD",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Gray,
+            )
+        }
+        Divider(color = Color.Black, thickness = 2.dp)
     }
 }
 
